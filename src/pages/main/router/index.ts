0d560bc50d26da1b1bc4f698a0997561a9e51d89
刘白菜 <liushuai.baicai@hotmail.com>
2022-07-11 18:26:00 +0800

    动态路由相关配置

--- src/pages/main/router/index.ts
+++ src/pages/main/router/index.ts
@@ -1,22 +1,9 @@
 import NProgress from "nprogress";
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: () => import("../views/Home.vue"),
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue"),
-  },
-];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition;
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,16 +1,26 @@
 import { defineStore } from "pinia";
+import routerBuild from "@/pages/main/router/route";
+import { RouteRecordNormalized } from "vue-router";
 
 export const useIndexStore = defineStore("index", {
   state: () => {
     return {
       pageData: Object.create(null),
+      routes: [] as RouteRecordNormalized[],
     };
   },
-  // could also be defined as
-  // state: () => ({ count: 0 })
   actions: {
     setPageData(key: string, value: any) {
       this.pageData[key] = value;
+    },
+    getRoutes() {
+      return new Promise((resolve) => {
+        const routes = routerBuild();
+        setTimeout(() => {
+          this.routes = routes;
+          resolve(routes);
+        }, 1000);
+      });
     },
   },
 });
